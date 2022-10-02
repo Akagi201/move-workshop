0afd40a0dbc24263e32c33ff45fe77ed36a5db5e
Akagi201 <akagi201@gmail.com>
2022-10-02 15:40:15 +0800

    feat(scripts): update docs

--- scripts/README.md
+++ scripts/README.md
@@ -10,6 +10,15 @@ python faucet.py
 ## Free proxy list
 
 * <http://free-proxy.cz/en/proxylist/country/CN/http/uptime/all>
+* <https://openproxy.space/list/http>
+* <https://www.kuaidaili.com/free/>
+* <http://proxylist.fatezero.org/>
+* <https://github.com/jhao104/proxy_pool#免费代理源>
+
+## Paid proxy list
+
+* <https://www.zdaye.com/CotenancyProxy.html>
+* <https://www.kuaidaili.com/>
 
 ## Online proxy check
 
--- scripts/faucet.py
+++ scripts/faucet.py
@@ -2,6 +2,8 @@
 import argparse
 import requests
 import sys
+# import time
+
 
 def send_request(proxy):
     proxies = {
@@ -9,8 +11,16 @@ def send_request(proxy):
         "https": "http://"+proxy,
     }
 
+    # username = "202210021334586276"
+    # password = "73597761"
+    # proxies = {
+    #     "http": "http://%(user)s:%(pwd)s@%(proxy)s" % {"user": username, "pwd": password, "proxy": proxy},
+    #     "https": "http://%(user)s:%(pwd)s@%(proxy)s" % {"user": username, "pwd": password, "proxy": proxy}
+    # }
+
     requests.packages.urllib3.disable_warnings()
     num = 9
+    success = 0
     for _ in range(num):
         try:
             response = requests.post(
@@ -21,9 +31,12 @@ def send_request(proxy):
             if response.status_code != 200:
                 break
             print(response.status_code, response.content)
+            success += 1
+            # time.sleep(1)
         except requests.exceptions.RequestException as e:
             print(proxy, e)
             break
+    return success
 
 
 def parse_args():
@@ -31,11 +44,15 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 def main(args=None):
     proxy_content = open("proxy_list.txt", "r")
     proxy_list = [line.rstrip() for line in proxy_content.readlines()]
+    total_success = 0
     for proxy in proxy_list:
-        send_request(proxy)
+        total_success += send_request(proxy)
+    print("Total success:", total_success)
+
 
 if __name__ == '__main__':
     sys.exit(main(parse_args()))
